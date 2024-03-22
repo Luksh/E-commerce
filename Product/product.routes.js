5d5e3e77069f88fba2bb52ff96537779975e8c9b
Laxman Maharjan <hotmale_luksh@hotmail.com>
2024-03-22 16:40:29 +0545

    Create Product

--- Product/product.routes.js
+++ Product/product.routes.js
@@ -1,5 +1,24 @@
 import express from "express";
+import { isSeller } from "../Middleware/authentication.middleware.js";
+import validateReqBody from "../Middleware/validation.middleware.js";
+import { addProductValidation } from "./product.validation.js";
+import Product from "./product.model.js";
 
 const router = express.Router();
+
+router.post("/product/create", isSeller, validateReqBody(addProductValidation), async (req, res) => {
+  // extract new product from req.body
+  const newProduct = req.body;
+
+  // extract loggedInUserId
+  const loggedInUserId = req.loggedInUserId;
+
+  newProduct.sellerId = loggedInUserId;
+
+  // create product
+  await Product.create(newProduct);
+
+  return res.status(200).send({ message: "Product is added successfully." });
+});
 
 export default router;
---
+++ Product/product.validation.js
@@ -0,0 +1,39 @@
+import Yup from "yup";
+
+export const addProductValidation = Yup.object({
+  name: Yup.string().trim().required("Name is required.").max(50, "Name should be less than 50 characters."),
+  brand: Yup.string().trim().required("Brand is required.").max(50, "Brand should be less than 50 characters."),
+  price: Yup.number().required("Price is required.").min(0, "Price should not be less than 0."),
+  category: Yup.string()
+    .trim()
+    .required("Category is required.")
+    .oneOf([
+      "Electronics",
+      "Clothing",
+      "Shoes",
+      "Books",
+      "Home",
+      "Beauty",
+      "Toys",
+      "Sports",
+      "Outdoors",
+      "Tools",
+      "Grocery",
+      "Health",
+      "Automotive",
+      "Industrial",
+      "Handmade",
+      "Jewelry",
+      "Music",
+      "Furniture",
+      "Kids",
+      "Other",
+    ]),
+  freeShipping: Yup.boolean().default(false),
+  availableQuantity: Yup.number().integer().required().min(1, "Quantity should be greater than 0."),
+  description: Yup.string()
+    .trim()
+    .required("Description is required.")
+    .min(100, "Description must be at least 100 characters.")
+    .max(1000, "Description should not be greater than 1000 characters."),
+});
