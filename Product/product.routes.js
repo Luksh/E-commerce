961c4fed3f20b2a7ef3569a994f80ec4d5f8fdd0
Laxman Maharjan <hotmale_luksh@hotmail.com>
2024-03-25 13:41:02 +0545

    CRUD

--- Product/product.routes.js
+++ Product/product.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { isSeller } from "../Middleware/authentication.middleware.js";
+import { isSeller, isUser } from "../Middleware/authentication.middleware.js";
 import validateReqBody from "../Middleware/validation.middleware.js";
 import { addProductValidation } from "./product.validation.js";
 import Product from "./product.model.js";
+import validateIdFromReqParams from "../Middleware/valid.params.middleware.js";
 
 const router = express.Router();
 
@@ -19,6 +20,41 @@ router.post("/product/create", isSeller, validateReqBody(addProductValidation), 
   await Product.create(newProduct);
 
   return res.status(200).send({ message: "Product is added successfully." });
+});
+
+router.get("/product/details/:id", isUser, validateIdFromReqParams, async (req, res) => {
+  const productId = req.params.id;
+
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    return res.status(404).send({ message: "Product does not exist." });
+  }
+
+  return res.status(200).send({ message: "Success", productDetails: product });
+});
+
+router.delete("/product/delete/:id", isSeller, validateIdFromReqParams, async (req, res) => {
+  const productId = req.params.id;
+
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    return res.status(404).send({ message: "Product does not exist." });
+  }
+
+  const sellerId = product.sellerId;
+  const loggedInUserId = req.loggedInUserId;
+
+  const isProductOwner = sellerId.equals(loggedInUserId);
+
+  if (!isProductOwner) {
+    return res.status(403).send({ message: "You are not authorized to delete this product." });
+  }
+
+  await Product.deleteOne({ _id: productId });
+
+  return res.status(200).send({ message: "Product deleted successfully." });
 });
 
 export default router;
